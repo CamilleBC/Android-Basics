2805f26d3fcac698fd4d34b73aacfde87da35a59
Camille Baillat <camille.baillat@pm.me>
2019-04-08 13:23:51 +0200

    Add fragments
    Two fragments, DogListFragment and DogEditorFragment, are managed by the MainActivity.
    
    They communicate using callbacks (`interfaces` from the *fragments* that are implemented in the *activity* that calls them).

--- app/src/main/java/me/camillebc/androidbasics/MainActivity.kt
+++ app/src/main/java/me/camillebc/androidbasics/MainActivity.kt
@@ -1,12 +1,68 @@
 package me.camillebc.androidbasics
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import me.camillebc.androidbasics.fragment.DogEditorFragment
+import me.camillebc.androidbasics.fragment.DogListFragment
 
-class MainActivity : AppCompatActivity() {
+private const val MAIN_ACTIVITY_BACKSTACK = "main_activity"
 
+/**
+ * The [MainActivity] implements the necessary callbacks from [DogEditorFragment] and [DogListFragment]
+ *
+ * It adds the [DogListFragment] on [MainActivity.onCreate] and then just waits until there is a callback from one of
+ * the fragments.
+ *
+ * See the official android documentation for more information:
+ * [Communicating with Other Fragments](http://developer.android.com/training/basics/fragments/communicating.html)
+ */
+class MainActivity : AppCompatActivity(),
+    DogEditorFragment.OnCancelClickListener,
+    DogListFragment.OnListFragmentInteractionListener,
+    DogListFragment.OnAddClickListener {
+
+    /**
+     * This function is the mandatory implementation of the [DogListFragment.OnAddClickListener] interface.
+     *
+     * We call [getSupportFragmentManager] and add a transaction to replace the existing fragment by
+     * [DogEditorFragment] on the button callback ([DogListFragment.activityCallback])
+     */
+    override fun onAddClick() {
+        Toast.makeText(this, "TODO: not implemented", Toast.LENGTH_SHORT).show()
+
+        val dogEditorFragment = DogEditorFragment()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.constraintLayout_main_fragment, dogEditorFragment)
+            .addToBackStack(MAIN_ACTIVITY_BACKSTACK)
+            .commit()
+        Log.i("FragmentManager: ", supportFragmentManager.fragments.toString())
+    }
+
+    /**
+     * This function is the mandatory implementation of the [DogEditorFragment.OnCancelClickListener] interface.
+     *
+     * We call [onBackPressed] to simulate a press on the back button.
+     */
+    override fun onCancelClick() {
+        onBackPressed()
+    }
+    override fun onListFragmentInteraction() {
+        TODO("not implemented") // There is no list yet to interact with
+    }
+
+    /**
+     * We call [getSupportFragmentManager] and add a transaction to start the [DogListFragment] on [MainActivity.onCreate]
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val dogListFragment = DogListFragment()
+        supportFragmentManager.beginTransaction()
+            .add(R.id.constraintLayout_main_fragment, dogListFragment)
+            .addToBackStack(MAIN_ACTIVITY_BACKSTACK)
+            .commit()
     }
 }
---
+++ app/src/main/java/me/camillebc/androidbasics/fragment/DogEditorFragment.kt
@@ -0,0 +1,42 @@
+package me.camillebc.androidbasics.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_dog_editor.*
+import me.camillebc.androidbasics.R
+
+
+class DogEditorFragment : Fragment() {
+    private lateinit var activityCallback: OnCancelClickListener
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_dog_editor, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        activity?.let {
+            activityCallback = activity as OnCancelClickListener
+            button_dogEditor_cancel.setOnClickListener { activityCallback.onCancelClick() }
+        }
+    }
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     *
+     * See the Android Training lesson
+     * [Communicating with Other Fragments](http://developer.android.com/training/basics/fragments/communicating.html)
+     * for more information.
+     */
+    interface OnCancelClickListener {
+        fun onCancelClick()
+    }
+}
